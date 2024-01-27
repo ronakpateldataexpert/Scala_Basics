43e57f6895c0594fbbcf26fcdfc7b408637479ac
ronak <ronakpateldataexpert@gmail.com>
2024-01-27 23:44:17 +0530

    Poc coding11

--- src/main/scala/Q5.scala
+++ src/main/scala/Q5.scala
@@ -3,6 +3,8 @@ object Q5 {
   // If not in range print invalid input.
   // Else – if the number is in range 90-100 then print Super Smart,
   // 80-90 print Smart,70-80 print smart enough,
+  //60-70 print just smart, 35-60 print no smart, 0-35 print dump.
+
 
   def main(args: Array[String]): Unit = {
     print(" Enter num: ")
@@ -16,6 +18,10 @@ object Q5 {
         println("Smart")
       } else if (num >= 70 && num < 80) {
         println("Smart  enough")
+      } else if (num >= 35 && num < 60) {
+        println("No Smart")
+      } else if (num >= 0 && num < 35) {
+        println("dump")
       }
     }
     else {
