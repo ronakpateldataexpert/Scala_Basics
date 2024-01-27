d0640bedaf70e7cd118ef4cf71b59fe51401586d
ronak <ronakpateldataexpert@gmail.com>
2024-01-28 00:39:07 +0530

    Q17

---
+++ src/main/scala/Q17.scala
@@ -0,0 +1,9 @@
+object Q17 {
+  def main(args: Array[String]): Unit = {
+
+    //17. Write a Program to print the all alphabets by using character Variable?
+
+    for (character <- 'A' to 'Z') {
+      print( character )
+  }
+}}
