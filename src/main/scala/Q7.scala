f888f1a26e031cea7196f06c2a9763b8e49f0456
ronak <ronakpateldataexpert@gmail.com>
2024-01-27 23:55:10 +0530

    Q7

---
+++ src/main/scala/Q7.scala
@@ -0,0 +1,8 @@
+object Q7 {
+  def main(args:Array[String]):Unit= {
+//7. Write a program to print “SEEKHO BIGDATA”for 60 times.
+    for (i<-1 to 60 ) {
+      println("SEEKHO BIGDATA")
+    }
+}
+}
