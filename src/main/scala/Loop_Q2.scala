80a5d2e2192f71c13110daee84cf6d71f1f5cd73
ronak <ronakpateldataexpert@gmail.com>
2024-01-28 23:02:50 +0530

    Loop Q2

---
+++ src/main/scala/Loop_Q2.scala
@@ -0,0 +1,11 @@
+object Loop_Q2 {
+  def main(args: Array[String]): Unit = {
+//2)Print even numbers from 2 to 10 using a "while" loop.
+    var i = 2
+    while(i<10){
+    if(i%2==0) {println(i)}
+      i=i+1
+    }
+  }
+
+  }
