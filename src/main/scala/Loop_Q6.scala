8263c712f07bb66855e726715ccb711c6275fb57
ronak <ronakpateldataexpert@gmail.com>
2024-01-28 23:22:52 +0530

    Loop Q6

---
+++ src/main/scala/Loop_Q6.scala
@@ -0,0 +1,11 @@
+object Loop_Q6 {
+  def main(args: Array[String]): Unit = {
+    //6)Generate and print the first 5 multiples of 3 using a "for" loop.
+
+    for(i<-1 to 5)
+      {
+        var s=i*3
+        println(s)
+      }
+  }
+}
