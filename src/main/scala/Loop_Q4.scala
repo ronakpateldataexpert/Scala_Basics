90093f914ef07c75710ddf4e44e000581ca5265a
ronak <ronakpateldataexpert@gmail.com>
2024-01-28 23:16:28 +0530

    Loop Q4

---
+++ src/main/scala/Loop_Q4.scala
@@ -0,0 +1,13 @@
+object Loop_Q4 {
+  def main(args: Array[String]): Unit = {
+
+    //4)Print the square of numbers from 1 to 5 using a "for" loop.
+
+    for(i<-1 to 5)
+      {
+        var sq=i*i
+        println(sq)
+
+      }
+  }
+}
